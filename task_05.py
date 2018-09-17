95c3672c506ef3cd611ddc57bf0572b88ce50cb2
brendamataflores82 <42910354+brendamataflores82@users.noreply.github.com>
2018-09-16 21:39:18 -0400

    Update task_05.py

--- task_05.py
+++ task_05.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """Splinter would be proud."""
-
-
+splint(TEENAGE_MUTANT_NINJAS)len() 
 TEENAGE_MUTANT_NINJAS = ('Michaelangelo. Leonardo. Rafael. Donatello. Heroes '
                          'in a half shell.')
+saved WORDCT
+PRINT WORDCT
