65a3c18486e3a2c3bfa67d4a96f4cb2526837ede
brendamataflores82 <42910354+brendamataflores82@users.noreply.github.com>
2018-09-16 21:51:32 -0400

    Update task_09.py

--- task_09.py
+++ task_09.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """A computer that can build the question."""
-
+import task_09.py
 THE_ANSWER_TO_EVERYTHING = 42
-
+help(task_09)
 INFINITE_IMPROBABILITY = 'browning motion'
+q
