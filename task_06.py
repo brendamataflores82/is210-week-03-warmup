61b14606ae39be048c78165db7315aefd56c7fce
brendamataflores82 <42910354+brendamataflores82@users.noreply.github.com>
2018-09-16 21:45:08 -0400

    Update task_06.py

--- task_06.py
+++ task_06.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """A very long book."""
-
+split()
 import os
-
+len()
 DPATH = os.path.dirname(os.path.abspath(__file__))
-
 FHANDLER = open(os.path.join(DPATH, 'war_and_peace.txt'), 'r')
-
 WORDS = FHANDLER.read()
-
 FHANDLER.close()
+WORDCT
+PRINT WORDCT
