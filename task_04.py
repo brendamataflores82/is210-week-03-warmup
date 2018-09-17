22ebb2895dace71e1b6572d4f207b5a99c67ea15
brendamataflores82 <42910354+brendamataflores82@users.noreply.github.com>
2018-09-16 21:28:55 -0400

    Update task_04.py

--- task_04.py
+++ task_04.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """Repeat our danger."""
-
 import task_03
-
-
+import task_03
+import task_03
+import task_03
+import task_03
 KLAXON = task_03.KLAXON
+print KLANOX
